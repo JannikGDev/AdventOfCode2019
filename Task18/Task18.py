30df056cdcb8fe1aec0c4b6960687c37c3deeb34
JannikG <jannikgroeger@gmx.net>
2019-12-20 19:38:49 +0100

    Failed at solving 18.1

--- Task18/Task18.py
+++ Task18/Task18.py
@@ -1,35 +1,49 @@
 import numpy as np
 from PQueue import PQueue
+import matplotlib.pyplot as plt
 
 
 START = ord("@")
 WALL = ord("#")
 EMPTY = ord(".")
 
-
 def task18():
 
     data = read_input("input.txt")
 
-    map = np.zeros((len(data[0]), len(data))).astype(np.int)
+    field = np.zeros((len(data[0]), len(data))).astype(np.int)
 
     for y, line in enumerate(data):
         for x, val in enumerate(line):
             if not val.isspace():
-                map[x, y] = int(ord(val))
+                field[x, y] = int(ord(val))
 
-    start_position = np.where(map == START)
+    display_map(field)
+
+    field = remove_deadends(field)
+
+    display_map(field)
+
+    start_position = np.where(field == START)
     start_position = [start_position[0][0], start_position[1][0]]
 
     keys = []
-    for x in range(len(map)):
-        for y in range(len(map[x])):
-            symbol = chr(map[x, y])
+    for x in range(len(field)):
+        for y in range(len(field[x])):
+            symbol = chr(field[x, y])
             if not symbol in ['#', '@', '.'] and symbol.islower():
-                keys.append(chr(map[x, y]))
+                keys.append(chr(field[x, y]))
 
-    path_instances = [([], 0)]
+    no_keys = dict()
+
+    for key in keys:
+        no_keys[key] = False
+
+    path_instances = [([], 0, None)]
+    iterations = 0
     while True:
+        n_path_instances = []
+
         if len(path_instances[0][0]) == len(keys):
             break
 
@@ -40,28 +54,102 @@ def task18():
                 if key in instance[0]:
                     continue
 
-                key_position = np.where(map == ord(key))
+                key_position = np.where(field == ord(key))
                 key_position = [key_position[0][0], key_position[1][0]]
 
                 doors = [door.upper() for door in instance[0]]
 
-                path = a_star(map, start_position, key_position, ['.','@'] + doors + [key] + instance[0])
+                path = a_star(field, start_position, key_position, ['.', '@'] + doors + [key] + instance[0])
 
                 if path is None:
                     continue
 
                 steps = path[0][1]
 
-                new_instance = (instance[0] + [key], instance[1]+steps)
+                new_instance = (instance[0] + [key], instance[1] + steps, key)
 
-                del path_instances[i]
-                path_instances.append(new_instance)
+                n_path_instances.append(new_instance)
 
+        path_instances = strip_bad_instances(n_path_instances)
+        iterations += 1
+        print(iterations)
 
     for instance in path_instances:
         print(instance)
 
     return
+
+
+def strip_bad_instances(instances):
+
+    to_be_removed = []
+
+    for i, A in enumerate(instances):
+        for j, B in enumerate(instances):
+
+            if i != j and A[2] == B[2] and equal_lists(A[0], B[0]):
+
+                if A[1] > B[1]:
+                    to_be_removed.append(i)
+                    break
+
+    for i in to_be_removed:
+        del instances[i]
+
+    return instances
+
+
+def remove_deadends(field):
+    change = True
+    while change:
+
+        change = False
+        for x in range(1, len(field) - 1):
+            for y in range(1, len(field[x]) - 1):
+                if field[x, y] == ord('.'):
+                    access = 0
+                    if field[x + 1, y] != ord('#'):
+                        access += 1
+                    if field[x, y + 1] != ord('#'):
+                        access += 1
+                    if field[x - 1, y] != ord('#'):
+                        access += 1
+                    if field[x, y - 1] != ord('#'):
+                        access += 1
+
+                    if access <= 1:
+                        field[x, y] = ord('#')
+                        change = True
+
+    return field
+
+
+def display_map(field):
+
+    plt.imshow(field)
+    plt.show()
+
+
+def equal_lists(A, B):
+
+    dict_A = dict()
+    dict_B = dict()
+
+    for a in A:
+        dict_A[a] = True
+    for b in B:
+        dict_B[b] = True
+
+    return equal_dict(dict_A, dict_B)
+
+
+def equal_dict(A, B):
+
+    for key in A.keys():
+        if not key in B:
+            return False
+
+    return True
 
 
 def heuristic(A, B):
@@ -72,7 +160,7 @@ def heuristic(A, B):
     return dx*dx+dy*dy
 
 
-def a_star(map, start, end, allowed_symbols):
+def a_star(field, start, end, allowed_symbols):
 
     q = PQueue()
 
@@ -83,7 +171,7 @@ def a_star(map, start, end, allowed_symbols):
     )
 
     q.put(heuristic(start_node[0], end), start_node)
-    visited_map = np.zeros(map.shape)
+    visited_map = np.zeros(field.shape)
 
     end_node = None
 
@@ -105,7 +193,7 @@ def a_star(map, start, end, allowed_symbols):
             x = node[0][0]+move[0]
             y = node[0][1]+move[1]
 
-            if chr(map[x, y]) in allowed_symbols and visited_map[x, y] == 0:
+            if chr(field[x, y]) in allowed_symbols and visited_map[x, y] == 0:
 
                 next_node = (
                     [x, y],
